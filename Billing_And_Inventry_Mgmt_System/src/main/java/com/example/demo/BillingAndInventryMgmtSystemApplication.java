3a09ef9bb1185bde02eb10eba5b9e0c6eaab2c03
AtharvLokhande <121097976+AtharvLokhande@users.noreply.github.com>
2025-03-31 11:11:46 +0530

    project Added to  main
    code pushed by Atharv

---
+++ Billing_And_Inventry_Mgmt_System/src/main/java/com/example/demo/BillingAndInventryMgmtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingAndInventryMgmtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingAndInventryMgmtSystemApplication.class, args);
+	}
+
+}
